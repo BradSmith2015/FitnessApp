0cf037792db73d7ae4108bec1d7044fbdeb64b1e
Peter829 <pest7240@colorado.edu>
2017-05-01 15:00:37 -0600

    added double line test

--- FitnessApp/app/src/main/java/com/example/android/fitnessapp/ProgressPage.java
+++ FitnessApp/app/src/main/java/com/example/android/fitnessapp/ProgressPage.java
@@ -1,33 +1,62 @@
 package com.example.android.fitnessapp;
 
+import android.graphics.Color;
 import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
-public class ProgressPage extends AppCompatActivity {
+import java.util.ArrayList;
 
-    LineGraphSeries<DataPoint> series;
+public class ProgressPage extends AppCompatActivity {
+    LineChart lineChart;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_progress_page);
 
-        double y,x;
-        x = -5.0;
+        lineChart = (LineChart) findViewById(R.id.lineChart);
 
-        GraphView graph = (GraphView) findViewById(R.id.graph);
-        series = new LineGraphSeries<DataPoint>();
-        for(int i = 0; i < 500; i++)
-        {
-            x = x + 0.1;
-            y = Math.sin(x);
-            series.appendData(new DataPoint(x, y), true, 500);
+        ArrayList<String> xAXES = new ArrayList<>();
+        ArrayList<Entry> yAXESsin = new ArrayList<>();
+        ArrayList<Entry> yAXEScos = new ArrayList<>();
+        double x = 0;
+        int numDataPoints = 1000;
+        for(int i = 0; i < numDataPoints; i++) {
+            float sinFunction = Float.parseFloat(String.valueOf(Math.sin(x)));
+            float cosFunction = Float.parseFloat(String.valueOf(Math.cos(x)));
+            x = x +0.1;
+            yAXESsin.add(new Entry(sinFunction,i));
+            yAXEScos.add(new Entry(cosFunction,i));
+            xAXES.add(i, String.valueOf(x));
         }
-        graph.addSeries(series);
+        String[] xaxes = new String[xAXES.size()];
+        for(int i = 0; i < xAXES.size(); i++)
+        {
+            xaxes[i] = xAXES.get(i).toString();
+        }
+        ArrayList<ILineDataSet> lineDataSets = new ArrayList<>();
+        LineDataSet lineDataSet1 = new LineDataSet(yAXEScos, "cos");
+        lineDataSet1.setDrawCircles(false);
+        lineDataSet1.setColor(Color.BLUE);
 
+        LineDataSet lineDataSet2 = new LineDataSet(yAXESsin, "sin");
+        lineDataSet2.setDrawCircles(false);
+        lineDataSet2.setColor(Color.RED);
+
+        lineDataSets.add(lineDataSet1);
+        lineDataSets.add(lineDataSet2);
+
+        lineChart.setData(new LineData(lineDataSets));
+        lineChart.setVisibleXRangeMaximum(65f);
     }
 }
